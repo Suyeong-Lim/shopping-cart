345b4a2c1c919542ee15cd6d2e04fa4c798d809d
Suyeong-Lim <tntn393@gmail.com>
2022-05-24 14:13:14 +0900

    fix: GlobalStyle, dto 수정. requirement 문서 추가

--- client/src/styles/GlobalStyles.ts
+++ client/src/styles/GlobalStyles.ts
@@ -3,27 +3,34 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
-    html,
-    body,
-    #root {
-    height: 100%;
-    }
-    * {
-    box-sizing: border-box;
-    }
-    a {
-    color: inherit;
-    text-decoration: none;
-    }
-    button {
-        background: inherit;
+      * {
+        box-sizing: border-box;
+      }
+      body{
+        font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+      }
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+      input, button {
+        background-color: transparent;
         border: none;
-        box-shadow:none;
-    }
-    
-    ol,ul,li {
-        list-style: none;
-    }
+        outline: none;
+      }
+      h1, h2, h3, h4, h5, h6{
+        font-family:'Maven Pro', sans-serif;
+      }
+      @media only screen and (max-width: 768px) {
+        body {
+          font-size: 12px;
+        }
+      }
+      @media only screen and (max-width: 576px) {
+        body {
+          font-size: 10px;
+        }
+      }
 `;
 
 export default GlobalStyles;
