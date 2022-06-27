ddc89744f610b4d03dd0cb34b9560de5f3f6ec8b
Suyeong-Lim <tntn393@gmail.com>
2022-06-28 01:02:21 +0900

    feat: 구매목록 반환 함수 추가

--- client/src/hooks/useCalcCartList.ts
+++ client/src/hooks/useCalcCartList.ts
@@ -1,30 +1,46 @@
-import { Cart } from "src/types/dto";
+import { Cart, OrderItem } from "src/types/dto";
 
-function useCalcCartList(data: Cart[]) {
-  const totalPrice = data.reduce((prev, cur) => {
+function useCalcCartList(cartList: Cart[]) {
+  const totalPrice = cartList.reduce((prev, cur) => {
     if (cur.product.selected) {
       return prev + cur.product.price * cur.product.quantity;
     }
     return prev;
   }, 0);
 
-  const totalCount = data.reduce((prev, cur) => {
+  const totalCount = cartList.reduce((prev, cur) => {
     if (cur.product.selected) {
       return prev + cur.product.quantity;
     }
     return prev;
   }, 0);
 
-  const cartSelectedIdList = data
+  const cartSelectedIdList = cartList
     .filter((cart) => cart.product.selected)
     .map((cart) => {
       return cart.id;
     });
 
-  const isSelectedAll = data.every((cart) => cart.product.selected);
+  const isSelectedAll = cartList.every((cart) => cart.product.selected);
 
+  // let count = 0;
+  const purchaseList: OrderItem[] = cartList.map((cart) => {
+    return {
+      id: cart.product.id,
+      price: cart.product.price,
+      name: cart.product.name,
+      imageUrl: cart.product.imageUrl,
+      quantity: cart.product.quantity,
+    };
+  });
 
-  return { totalPrice, totalCount, cartSelectedIdList, isSelectedAll };
+  return {
+    totalPrice,
+    totalCount,
+    cartSelectedIdList,
+    isSelectedAll,
+    purchaseList,
+  };
 }
 
 export default useCalcCartList;
