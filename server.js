80332543ef51df2e6039e6048b450864661ccae6
Suyeong-Lim <tntn393@gmail.com>
2022-06-28 01:43:26 +0900

    fix: port 번호수정

--- server.js
+++ server.js
@@ -294,6 +294,7 @@ server.post("/orders", (req, res) => {
 // default router
 server.use(router);
 
-server.listen(3003, () => {
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
   console.log("JSON Server is running");
 });
